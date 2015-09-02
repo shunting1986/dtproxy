30042c0575dd6b71ce5de5036f4bb1c93471e154
Shunting <zhangst1986@gmail.com>
2015-09-02 15:40:28 +0800

    write a simple http server using http.server module

--- src/server.py
+++ src/server.py
@@ -1,12 +1,48 @@
-from wsgiref.simple_server import make_server
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+import sys
 
-def req_handler(environ, start_response):
-	print("Enter req_handler") # TODO
+class HandlerClass(SimpleHTTPRequestHandler):
+	pass
 
 class Server:
 	def __init__(self, host, port):
-		self.httpd = make_server(host, port, req_handler)
-		print("Serving on port {0}".format(port))
+		self.host = host
+		self.port = port
 	
 	def start(self):
-		self.httpd.serve_forever()
+		httpd = HTTPServer((self.host, self.port), HandlerClass)
+		print("Serving on port {0}".format(self.port))
+
+		try:
+			httpd.serve_forever()
+		except KeyboardInterrupt:
+			print("\nKeyboard interrupt received, exiting.")
+			httpd.server_close()
+			sys.exit(0)
+
+### WSGI implementation
+# from wsgiref.simple_server import make_server
+# 
+# def req_handler(environ, start_response):
+# 	cl = environ.get("CONTENT_LENGTH", None)
+# 	for x in environ:
+# 		print(x)
+# 		print("\t{0}".format(environ[x]))
+# 	
+# 	if cl:
+# 		body = environ["wsgi.input"].read(int(cl))
+# 	else:
+# 		body = environ["wsgi.input"].read()
+# 	print("body:")
+# 	print(body)
+# 	print()
+# 
+# 	print("Enter req_handler") # TODO
+# 
+# class Server:
+# 	def __init__(self, host, port):
+# 		self.httpd = make_server(host, port, req_handler)
+# 		print("Serving on port {0}".format(port))
+# 	
+# 	def start(self):
+# 		self.httpd.serve_forever()
